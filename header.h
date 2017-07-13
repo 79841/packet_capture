3d5342411ae8b575e3c4f574644b81d85f4d5ac4
skj974600 <79841@naver.com>
2017-07-13 10:32:10 -0700

    Add header.h

---
+++ header.h
@@ -0,0 +1,25 @@
+struct ether_header
+{
+	unsigned char ether_dhost[6];      
+	unsigned char ether_shost[6];
+	unsigned short ether_type;
+};
+struct ip
+{
+	unsigned char dump[12];
+	unsigned char ip_src[4];
+	unsigned char ip_dst[4];
+};
+struct tcp
+{
+	unsigned char pt_src[2];
+	unsigned char pt_dst[2];
+	unsigned char dump[20];
+	unsigned char data[100];
+};
+struct pket
+{
+	struct ether_header ethhd;
+	struct ip iphd;
+	struct tcp tcphd;
+};
